e130f4692bb24fc3407cf6d15a9a9e8cb3478f85
Henry Gressmann <mail@henrygressmann.de>
2024-09-06 01:46:40 +0200

    fix: fix image csp for favicons
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -62,7 +62,7 @@ pub fn create_router(app: Liwan, events: Sender<Event>) -> impl IntoEndpoint {
         .appending("X-Frame-Options", "DENY")
         .appending("X-Content-Type-Options", "nosniff")
         .appending("X-XSS-Protection", "1; mode=block")
-        .appending("Content-Security-Policy", "default-src 'self' data: 'unsafe-inline'")
+        .appending("Content-Security-Policy", "default-src 'self' data: 'unsafe-inline'; img-src https://*")
         .appending("Referrer-Policy", "same-origin")
         .appending("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
 
