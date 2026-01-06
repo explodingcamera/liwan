3980ef25b904500911264048ea1b9a9731c8fcba
Henry Gressmann <mail@henrygressmann.de>
2026-01-06 19:34:14 +0100

    chore: finish all handlers
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- src/web/routes/auth.rs
+++ src/web/routes/auth.rs
@@ -7,26 +7,39 @@ use http::{StatusCode, header};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use tokio::task::spawn_blocking;
+use tower_governor::{GovernorLayer, governor::GovernorConfigBuilder};
 
 use crate::{
     app::models::UserRole,
     utils::hash::session_token,
     web::{
-        RouterState, SessionUser,
+        MaybeExtract, RouterState, SessionUser,
         session::{MAX_SESSION_AGE, SessionId},
         webext::{ApiResult, AxumErrExt, empty_response, http_bail},
     },
 };
 
 pub fn router() -> ApiRouter<RouterState> {
+    let limiter = GovernorConfigBuilder::default().per_second(2).burst_size(5).finish().expect("valid governor config");
+
+    let governor_limiter = limiter.limiter().clone();
+    tokio::task::spawn(async move {
+        let mut interval = tokio::time::interval(std::time::Duration::from_hours(1));
+        loop {
+            interval.tick().await;
+            governor_limiter.retain_recent();
+        }
+    });
+
     ApiRouter::new()
+        .layer(GovernorLayer::new(limiter))
         .api_route("/auth/me", get(me))
         .api_route("/auth/setup", post(setup))
         .api_route("/auth/login", post(login))
         .api_route("/auth/logout", post(logout))
 }
 
-#[derive(Serialize, JsonSchema)]
+#[derive(Deserialize, Serialize, JsonSchema)]
 pub struct LoginRequest {
     pub username: String,
     pub password: String,
@@ -44,10 +57,6 @@ pub struct MeResponse {
     pub username: String,
     pub role: UserRole,
 }
-
-// fn login_rate_limit(ep: impl Endpoint + 'static) -> impl Endpoint {
-//     ep.with(RateLimitLayer::new(10, std::time::Duration::from_secs(10)).compat())
-// }
 
 async fn me(SessionUser(user): SessionUser) -> impl IntoApiResponse {
     ([(header::CACHE_CONTROL, "private")], Json(MeResponse { username: user.username, role: user.role }))
@@ -70,8 +79,8 @@ async fn setup(app: State<RouterState>, Json(params): Json<SetupRequest>) -> Api
 
 async fn login(
     app: State<RouterState>,
+    _cookies: CookieJar,
     Json(params): Json<LoginRequest>,
-    cookies: &CookieJar,
 ) -> ApiResult<impl IntoApiResponse> {
     let username = params.username.clone();
 
@@ -89,32 +98,32 @@ async fn login(
     let expires = Utc::now() + MAX_SESSION_AGE;
     app.sessions.create(&session_id, &username, expires).http_status(StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    let mut public_cookie = PUBLIC_COOKIE.clone();
-    let mut session_cookie = SESSION_COOKIE.clone();
-    public_cookie.set_secure(app.config.secure());
-    public_cookie.set_value_str(username.clone());
-    session_cookie.set_secure(app.config.secure());
-    session_cookie.set_value_str(session_id);
-    cookies.add(public_cookie);
-    cookies.add(session_cookie);
+    // let mut public_cookie = PUBLIC_COOKIE.clone();
+    // let mut session_cookie = SESSION_COOKIE.clone();
+    // public_cookie.set_secure(app.config.secure());
+    // public_cookie.set_value_str(username.clone());
+    // session_cookie.set_secure(app.config.secure());
+    // session_cookie.set_value_str(session_id);
+    // cookies.add(public_cookie);
+    // cookies.add(session_cookie);
     Ok(empty_response())
 }
 
 async fn logout(
     app: State<RouterState>,
-    cookies: &CookieJar,
-    session_id: Option<SessionId>,
+    _cookies: CookieJar,
+    MaybeExtract(session_id): MaybeExtract<SessionId>,
 ) -> ApiResult<impl IntoApiResponse> {
     if let Some(session_id) = session_id {
         let _ = app.sessions.delete(&session_id.0);
     }
-    let mut public_cookie = PUBLIC_COOKIE.clone();
-    let mut session_cookie = SESSION_COOKIE.clone();
-    public_cookie.set_secure(app.config.secure());
-    public_cookie.make_removal();
-    session_cookie.set_secure(app.config.secure());
-    session_cookie.make_removal();
-    cookies.add(public_cookie);
-    cookies.add(session_cookie);
+    // let mut public_cookie = PUBLIC_COOKIE.clone();
+    // let mut session_cookie = SESSION_COOKIE.clone();
+    // public_cookie.set_secure(app.config.secure());
+    // public_cookie.make_removal();
+    // session_cookie.set_secure(app.config.secure());
+    // session_cookie.make_removal();
+    // cookies.add(public_cookie);
+    // cookies.add(session_cookie);
     Ok(empty_response())
 }
